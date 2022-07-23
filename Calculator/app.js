af3418880f0a6a9971ceaf55e683967e3274fa69
Seangmeng1408 <83820236+Seangmeng1408@users.noreply.github.com>
2022-07-23 14:22:53 +0700

    Update
    Calculator v2

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,56 +1,79 @@
+let input=document.querySelector(".input");
+let experssion=document.querySelector(".expression");
+let operation=document.querySelector("#operation");
+let Expression_Dsp="",Expression="";
 
 
+let Delete=document.querySelector("#delete");
 
-var el=document.getElementById("sub-container");
-var text=document.querySelector(".result")
-var target,value="",opt,eq;
-var result;
 
-el.addEventListener("click",(e)=>{
-    if(e.target.className){
-        target=e.target;
-        if(target.className=="clear"){
-            text.children[3].innerText='0';
-            text.children[0].innerText=""
-            result=0;
-            value=""
-        }
-        else{
-            if(target.className=="number"){
-                value+=target.id
-                text.children[3].innerText=value;
-                
+let value="";
+let lastOperand="",isOperand=false;
+operation.addEventListener("click",(e)=>{
+    let target=e.target;
+    if(target.classList.contains("number")){
+        isOperand=false;
+        lastOperand="";
+        Expression+=target.getAttribute("data-actual-value");
+        Expression_Dsp+=target.innerText;
+        value+=target.innerText;
+        input.innerText=value;
+    }
+    if(target.classList.contains("operand")){
+        if(Expression_Dsp!=""){
+            if(target.getAttribute("data-actual-value")=="c"){
+                Expression="";
+                Expression_Dsp="";
+                input.innerText="0";
+                experssion.innerText="0";
+                value="";
+            }else if(target.getAttribute("data-actual-value")=="d"){
+                Expression_Dsp=Expression_Dsp.slice(0,-1);
+                Expression=Expression.slice(0,-1);
+                if(value.length>1){
+                    //input.innerText=value;
+                    value=value.slice(0,-1);
+                    input.innerText=value;
+
+                }else{
+                    value=value.slice(0,-1);
+                    input.innerText="0";
+                }
+            }else if(target.getAttribute("data-actual-value")==")" ||target.getAttribute("data-actual-value")=="("){
+                Expression+=target.getAttribute("data-actual-value");
+                Expression_Dsp+=target.innerText;
+                experssion.innerText=Expression_Dsp;
+                input.innerText="0";
+                value="";
+            }else {
+                if(target.getAttribute("data-actual-value")!="="){
+                    if(!isOperand){
+                        Expression+=target.getAttribute("data-actual-value");
+                        Expression_Dsp+=target.innerText;
+                        experssion.innerText=Expression_Dsp;
+                        input.innerText="0";
+                        value="";
+                        isOperand=true;
+                    }else{
+                        Expression_Dsp=Expression_Dsp.slice(0,-1);
+                        Expression_Dsp+=target.innerText;
+                        Expression=Expression.slice(0,-1);
+                        Expression+=target.getAttribute("data-actual-value");
+                        experssion.innerText=Expression_Dsp;
+                    }
+                }else{
+                    experssion.innerText=Expression_Dsp;
+                    input.innerText=""+eval(Expression);
+                    Expression_Dsp=""+eval(Expression);
+                    Expression=""+eval(Expression);
+                    value=""+eval(Expression);
+                    //console.log(value);
+                }
             }
-            else if(target.className=="operator"){
-                opt=target.id;
-                
-              
-                console.log(result)
-                if(text.children[3].innerText!="0"){
-                    text.children[0].innerText=text.children[0].innerText+value+opt;
-                    text.children[3].innerText='0';
-                    value=""
-                    
-                };
-                
-                
-                
-            }
-            else if(target.className=="equal"){
-                text.children[3].innerText=eval(text.children[0].innerText+text.children[3].innerText);
-                text.children[0].innerText='';
-                value=eval(text.children[0].innerText+text.children[3].innerText)
-                
-            }
-            
-            
-            
-            
-            
+
         }
     }
-    
-    
-    
-    
 })
+
+
+
